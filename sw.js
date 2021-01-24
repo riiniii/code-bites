52cbfc0536814cbbdd5b59be7b02b9358c1bb3a9
riiniii <reneehuang23@gmail.com>
2021-01-24 16:27:13 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-014324eabadc22769ee5.js"
+    "url": "webpack-runtime-dedd3625de5cc831c2f2.js"
   },
   {
     "url": "styles.67d1bd6e7156b0307ab0.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ebd0d94b8c2ef8700d4ee1c0b737e6f4"
+    "revision": "4a70828050be00f2db5d2bd81fbe4fb7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e1b636f63713a0d3b6dce03cdb1252de"
+    "revision": "ed8ca5287ef15246387102dde83fc850"
   },
   {
     "url": "polyfill-0eca59d2ee8c0828e3a6.js"
